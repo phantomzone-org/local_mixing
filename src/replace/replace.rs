dd5e17b17d95aeccabef247a8ae24d4c6f9be713
Nicholas Ho <hon@bu.edu>
2025-11-05 00:13:00 -0500

    stop exhaust for now

--- src/replace/replace.rs
+++ src/replace/replace.rs
@@ -654,7 +654,8 @@ pub fn compress_big(c: &CircuitSeq, trials: usize, num_wires: usize, conn: &mut 
         // compress logs everything inside compress now
         //let t2 = Instant::now();
         let subcircuit_temp = if subcircuit.gates.len() <= 200 {
-            compress_exhaust(&subcircuit, conn, &bit_shuf, num_wires)
+            // compress_exhaust(&subcircuit, conn, &bit_shuf, num_wires)
+            compress(&subcircuit, 200, conn, &bit_shuf, num_wires)
         } else {
             println!("Too big for exhaust: Len = {}", subcircuit.gates.len());
             compress(&subcircuit, 25_000, conn, &bit_shuf, num_wires)
