88f3ea433386023017b3b17aeb3f6f547fc7a7fe
hon <hon@bu.edu>
2025-11-07 01:08:21 -0500

    larger identities

--- src/replace/mixing.rs
+++ src/replace/mixing.rs
@@ -360,7 +360,7 @@ pub fn abutterfly_big(
     let mut rng = rand::rng();
     let mut pre_blocks: Vec<CircuitSeq> = Vec::with_capacity(c.gates.len());
 
-    let (first_r, first_r_inv) = random_id(n as u8, rng.random_range(15..=25));
+    let (first_r, first_r_inv) = random_id(n as u8, rng.random_range(100..=200));
     let mut prev_r_inv = first_r_inv.clone();
 
     for &g in &c.gates {
