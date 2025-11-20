21bc79d03bcac43776c4d54380c9cb35058f8b0c
Nicholas Ho <hon@bu.edu>
2025-11-20 15:31:03 -0500

    more butterfly wings

--- src/replace/mixing.rs
+++ src/replace/mixing.rs
@@ -323,7 +323,7 @@ pub fn butterfly_big(
 ) -> CircuitSeq {
     // Pick one random R
     let mut rng = rand::rng();
-    let (r, r_inv) = random_id(n as u8, rng.random_range(50..=70)); 
+    let (r, r_inv) = random_id(n as u8, rng.random_range(100..=200)); 
     let mut c = c.clone();
     shoot_random_gate(&mut c, 500_000);
     println!("Butterfly start: {} gates", c.gates.len());
