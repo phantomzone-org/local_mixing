d47d54207dfe5ee2c631f47998aadb6b5eed9c49
hon <hon@bu.edu>
2025-12-19 15:40:54 -0500

    add panic to single gate repl

--- src/replace/replace.rs
+++ src/replace/replace.rs
@@ -1636,6 +1636,9 @@ fn random_gate_replacements(c: &mut CircuitSeq, x: usize, n: usize, _conn: &Conn
             }
             used_wires.sort();
             let rewired_g = CircuitSeq::rewire_subcircuit(&c, &vec![i], &used_wires);
+            if used_wires.len() != num {
+                panic!("used: {} num: {}", used_wires.len(), num);
+            }
             id.rewire_first_gate(rewired_g.gates[0], num);
             id = CircuitSeq::unrewire_subcircuit(&id, &used_wires);
             id.gates.remove(0);
