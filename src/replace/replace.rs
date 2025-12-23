10c2f09665c88e979a06e9f150e9e03e14a30df8
hon <hon@bu.edu>
2025-12-23 03:24:51 -0500

    fix calculation for single gates

--- src/replace/replace.rs
+++ src/replace/replace.rs
@@ -1505,7 +1505,7 @@ pub fn replace_pairs(circuit: &mut CircuitSeq, num_wires: usize, conn: &mut Conn
     }
 
     println!("Starting single gate replacements");
-    random_gate_replacements(circuit, (num_pairs - replaced)/20 + (m/2 - 2 * num_pairs)/20, num_wires, conn, env);
+    random_gate_replacements(circuit, (num_pairs - replaced)/20 + (m/2 - num_pairs)/20, num_wires, conn, env);
     println!("Finished replace_pairs");
 }
 
