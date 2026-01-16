82ded08ac9292f832a36adeedeb138cb4fb79ee5
hon <hon@bu.edu>
2026-01-15 23:36:31 -0500

    see if the gate repl is wrong

--- src/replace/replace.rs
+++ src/replace/replace.rs
@@ -1724,8 +1724,8 @@ pub fn replace_pairs(circuit: &mut CircuitSeq, num_wires: usize, conn: &mut Conn
                     let mut new_circuit = Vec::with_capacity(id.gates.len());
                     // Append gates after the pair
                     new_circuit.extend_from_slice(&id.gates[i + 2..]);
-                    // Append gates before the pair, in reverse
-                    new_circuit.extend(id.gates[0..i].iter().rev());
+                    // Append gates before the pair
+                    new_circuit.extend(id.gates[0..i].iter());
                     let nc = CircuitSeq { gates: new_circuit.clone() };
                     if nc.probably_equal(&CircuitSeq { gates: vec![id.gates[i], id.gates[i+1]]}, num_wires, 10000).is_err() { panic!("pairs dont match new"); }
                     to_replace[chosen / 2] = (new_circuit, vec![id.gates[i], id.gates[i+1]]);
@@ -1758,7 +1758,7 @@ pub fn replace_pairs(circuit: &mut CircuitSeq, num_wires: usize, conn: &mut Conn
                     // Append gates after the pair
                     new_circuit.extend_from_slice(&id.gates[i + 2..]);
                     // Append gates before the pair, in reverse
-                    new_circuit.extend(id.gates[0..i].iter().rev());
+                    new_circuit.extend(id.gates[0..i].iter());
                     let nc = CircuitSeq { gates: new_circuit.clone() };
                     if nc.probably_equal(&CircuitSeq { gates: vec![id.gates[i+1], id.gates[i]]}, num_wires, 10000).is_err() { panic!("reverse pairs dont match new"); }
                     to_replace[chosen / 2] = (new_circuit, vec![id.gates[i], id.gates[i+1]]);
